1030f56d2f5db53e7d0009f07cca0131d19f47dd
KamLynch <kambrialynch95@gmail.com>
2024-06-27 19:49:56 -0400

    add html, css, js file and succulent flower

---
+++ main.js
@@ -0,0 +1,5 @@
+words = ['organ', 'valve', 'heart', 'scapel', 'podiatrist', 'health', 'gloves', 'pulse', 'discharge']
+
+let randomIndex = Math.floor(Math.random() * words.length); 
+let selectedWord = words[randomIndex]; 
+console.log(selectedWord);
